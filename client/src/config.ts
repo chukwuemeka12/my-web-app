f5658d1a59dc89431e5cffc69fa78d7d706ff441
Chukwuemeka Cameron <ccameron@designprivacy.io>
2025-03-25 22:11:00 -0500

    Setup new Heroku app

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 // API configuration
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://1c1b1750-b456-4de3-b215-884e58fd75ee.herokuapp.com'
+  ? 'https://chi-dashboard-app.herokuapp.com'
   : 'http://localhost:5000';
